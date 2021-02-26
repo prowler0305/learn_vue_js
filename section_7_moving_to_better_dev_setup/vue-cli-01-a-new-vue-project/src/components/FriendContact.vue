497ef41e2e524b2bb66b7e3e43059cac684804ae
Andrew Spear <prowler0305@outlook.com>
2021-02-26 09:12:07 -0600

    Add section 7 app files.

---
+++ section_7_moving_to_better_dev_setup/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Hide':'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{friend.phone}}</li>
+      <li><strong>email:</strong>{{friend.email}}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      detailsAreVisible: false,
+      friend: {
+        id: 'william',
+        name: 'William Loza',
+        phone: '012-345-6789',
+        email: 'William.Loza@localhost.com'
+      },
+    };
+  },
+  methods:{
+    toggleDetails(){
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
