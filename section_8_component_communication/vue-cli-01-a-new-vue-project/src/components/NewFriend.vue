33d7a99627d0a15668bac729f02eb9644a922d99
Andrew Spear <prowler0305@outlook.com>
2021-03-01 19:00:04 -0600

    Add rest of section 8 files with" Friends" app.

---
+++ section_8_component_communication/vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,57 @@
+<template>
+  <header>
+    <h1>Add Friend</h1>
+  </header>
+  <form @submit.prevent="addContact">
+    <div>
+      <label>First Name:</label>
+      <input type="text" v-model="firstName"/>
+    </div>
+    <div>
+      <label>Last Name:</label>
+      <input type="text" v-model="lastName"/>
+    </div>
+    <div>
+      <label>Phone Number:</label>
+      <input type="text" v-model="phoneNumber"/>
+    </div>
+    <div>
+      <label>Email:</label>
+      <input type="text" v-model="emailAddr"/>
+    </div>
+    <button>Add Friend</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-friend'],
+  data(){
+    return {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      emailAddr: '',
+    };
+  },
+  methods:{
+    addContact(){
+      const newFriend = {
+        id: this.firstName,
+        name: this.firstName + ' ' + this.lastName,
+        phone: this.phoneNumber,
+        email: this.emailAddr
+      }
+      this.$emit('add-friend', newFriend)
+      this.firstName = ''
+      this.lastName = ''
+      this.phoneNumber = ''
+      this.emailAddr = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
