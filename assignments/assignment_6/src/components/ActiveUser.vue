6b18cc9bd3dc0dfcd7a8048d11e09bd6367bbc3c
Andrew Spear <prowler0305@outlook.com>
2021-03-03 14:30:56 -0600

    Add assignment 6 completed.

---
+++ assignments/assignment_6/src/components/ActiveUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h2>Your Name: {{name}}</h2>
+    <h3>Your Age: {{age}}</h3>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "ActiveUser",
+  props:{
+    name: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ assignments/assignment_6/src/components/UserData.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h2>Active User Input</h2>
+  </header>
+  <form @submit.prevent="processUserData">
+    <div>
+      <label>Name:</label>
+      <input type="text" v-model="enteredName"/>
+    </div>
+    <div>
+      <label>Age:</label>
+      <input type="text" v-model="enteredAge"/>
+    </div>
+    <button>Set User Data</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "UserData",
+  emits:['set-user-data'],
+  data(){
+    return{
+      enteredName: '',
+      enteredAge: null
+    }
+  },
+  methods:{
+    processUserData(){
+      this.$emit('set-user-data', this.enteredName, this.enteredAge)
+      this.enteredName = ''
+      this.enteredAge = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
